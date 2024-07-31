31636afd9857bb978171c4edd457622e7856653f
liuyuyan2717 <2717108404@qq.com>
2024-07-31 16:05:59 +0800

    feat：完成一个收发消息的客户端Client

--- main/main.go
+++ main/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+// 	"encoding/json"
 	"fmt"
 	"geerpc"
-	"geerpc/codec"
+// 	"geerpc/codec"
 	"log"
 	"net"
 	"time"
+	"sync"
 )
 
 func startServer(addr chan string) {
@@ -22,28 +23,26 @@ func startServer(addr chan string) {
 }
 
 func main() {
-	log.SetFlags(1)
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
-
-	// in fact, following code is like a simple geerpc client
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	// send options
-	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
