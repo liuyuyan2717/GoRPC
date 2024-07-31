c6af64884fc45f8fefdcc1f225630e7166ae7d31
liuyuyan2717 <2717108404@qq.com>
2024-07-31 16:53:46 +0800

    feat：反射实现调用具体方法

--- main/main.go
+++ main/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-// 	"encoding/json"
-	"fmt"
 	"geerpc"
-// 	"geerpc/codec"
 	"log"
 	"net"
-	"time"
 	"sync"
+	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := geerpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
 	}
 	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String() // 启动一个服务
+	addr <- l.Addr().String()
 	geerpc.Accept(l)
 }
 
@@ -36,12 +46,12 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
